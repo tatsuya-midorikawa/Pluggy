055de70b17143480938b587d320e4e98d092d843
midoliy <midorikawa@tech-leaves.com>
2021-03-03 20:07:47 +0900

    💡PlugクラスにActivateAsync()を追加

--- src/Pluggy/Plug.cs
+++ src/Pluggy/Plug.cs
@@ -1,6 +1,9 @@
 ﻿using Reffy.Expressions;
 using System;
 using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Pluggy
 {
@@ -13,6 +16,21 @@ namespace Pluggy
             this.type = type;
         }
 
+        private static bool Same(Type[] types, ConstructorInfo ctor)
+        {
+            var parameters = ctor.GetParameters();
+            if (types.Length != parameters.Length)
+                return false;
+
+            for (int i = 0; i < types.Length; i++)
+            {
+                if (types[i] != parameters[i].ParameterType)
+                    return false;
+            }
+
+            return true;
+        }
+
         public T Activate<T>(params object[] args)
         {
             var t = typeof(T);
@@ -22,8 +40,26 @@ namespace Pluggy
             if (!type.GetInterfaces().Any(type => type == t))
                 throw new Exception($"{type.FullName} must implement {t.FullName}.");
 
-            return (T)type.Constructor(args);
+            if (args == null)
+                return (T)type.Constructor();
+
+            var types = args.Select(arg => arg.GetType()).ToArray();
+            return type.GetConstructors().Any(ctor => Same(types, ctor))
+                ? (T)type.Constructor(args)
+                : (T)type.Constructor();
         }
+
+        public async Task<T> ActivateAsync<T>(params object[] args)
+            => await Task.Factory.StartNew(() => Activate<T>(args));
+
+        public async Task<T> ActivateAsync<T>(CancellationToken cancellationToken, params object[] args)
+            => await Task.Factory.StartNew(() => Activate<T>(args), cancellationToken);
+
+        public async Task<T> ActivateAsync<T>(TaskCreationOptions creationOptions, params object[] args)
+            => await Task.Factory.StartNew(() => Activate<T>(args), creationOptions);
+
+        public async Task<T> ActivateAsync<T>(CancellationToken cancellationToken, TaskCreationOptions creationOptions, TaskScheduler scheduler, params object[] args)
+            => await Task.Factory.StartNew(() => Activate<T>(args), cancellationToken, creationOptions, scheduler);
     }
 
 }
